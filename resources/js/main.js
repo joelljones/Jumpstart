bb21fa37f4d5255d182d6d2e55ef3a6660c8a20d
Joell Jones <joelljones86@gmail.com>
2017-10-11 18:40:06 -0400

    Hide nav bar

--- resources/js/main.js
+++ resources/js/main.js
@@ -8,4 +8,18 @@ $(document).ready(function() {
     pauseOnFocus: false,
     pauseOnHover: false
   });
+
+  // hide the navigation bar as the user scrolls
+  var userScroll = 0;
+  $(window).scroll(function() {
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - userScroll > 50) {
+      var navbarHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navbarHeight}, 150);
+      userScroll = scrollTop;
+    } else if (userScroll - scrollTop > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      userScroll = scrollTop;
+    }
+  });
 });
